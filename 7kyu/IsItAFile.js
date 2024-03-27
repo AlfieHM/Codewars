eee3657a89f97e5806268bc646b674d63567af03
AlfieHM <161490513+AlfieHM@users.noreply.github.com>
2024-03-27 13:24:39 +0000

    Create IsItAFile.js
    Return true if character is a letter

---
+++ 7kyu/IsItAFile.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+
+function isItLetter(character) {
+  return character.toLowerCase()!==character.toUpperCase()
+}
