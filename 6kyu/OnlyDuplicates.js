917f9fca0d18e8d6a4a6ce0331e778d9efd8288d
AlfieHM <161490513+AlfieHM@users.noreply.github.com>
2024-07-07 21:15:06 +0100

    Create OnlyDuplicates.js

---
+++ 6kyu/OnlyDuplicates.js
@@ -0,0 +1,26 @@
+// Given a string, remove any characters that are unique from the string.
+
+// Example:
+
+// input: "abccdefee"
+
+// output: "cceee"
+
+function onlyDuplicates(str) {
+  let arr = str.split('')
+  let arr1 = []
+  for(let i=0; i<arr.length; i++){
+    let val = arr[i]
+    let count=0
+      for(let y=0; y<arr.length; y++){
+      
+       if(val===arr[y]){
+        count++
+        }
+      }
+    if(count>1){
+        arr1.push(arr[i])
+      }
+  }
+  return arr1.join('')
+}
