2d12c9be6109c1f21d73c3b7e4a69e70a8ba3cd4
AlfieHM <161490513+AlfieHM@users.noreply.github.com>
2024-10-05 16:19:50 +0100

    Update Vaporcode.js

--- 7kyu/Vaporcode.js
+++ 7kyu/Vaporcode.js
@@ -1,1 +1,11 @@
+// Description:
+// ASC Week 1 Challenge 4 (Medium #1)
 
+// Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+// Note that spaces should be ignored in this case.
+
+function vaporcode(string) {
+  let array = string.toUpperCase().split('').filter((x)=>x!==" ").join('  ')
+  return array
+}
